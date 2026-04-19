543d62a0ad579b168e71e539ce69122123af35ae
1689863234 <1689863234@qq.com>
2026-04-19 14:19:03 +0800

    feat(page): export MfwPageWrapper and MfwListPage, backward compat for MfwPageScene

--- packages/base-frontend/src/components/page/index.ts
+++ packages/base-frontend/src/components/page/index.ts
@@ -2,4 +2,9 @@
  * @fileoverview 页面类组件导出
  */
 
-export * from './page-scene/mod';
+export * from './page-wrapper/mod';
+export * from './list-page/mod';
+
+// 向后兼容：MfwPageScene 指向 MfwListPage
+export { MfwListPage as MfwPageScene } from './list-page/mod';
+export type * from './list-page/types';
