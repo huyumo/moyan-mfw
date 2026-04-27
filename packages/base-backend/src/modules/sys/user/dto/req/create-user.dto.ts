e62cd275cfe90265e61c362be35fab46749eca90
1689863234 <1689863234@qq.com>
2026-04-27 17:16:54 +0800

    fix: update backend avatar field types to ImageResource

--- packages/base-backend/src/modules/sys/user/dto/req/create-user.dto.ts
+++ packages/base-backend/src/modules/sys/user/dto/req/create-user.dto.ts
@@ -60,10 +60,9 @@ export class CreateUserDto {
   /**
    * 头像
    */
-  @ApiProperty({ description: '头像 URL', required: false })
+  @ApiProperty({ description: '头像', required: false })
   @IsOptional()
-  @IsString()
-  avatar?: string;
+  avatar?: { src: string; width: number; height: number };
 
   /**
    * 性别 (0:未知 1:男 2:女)
