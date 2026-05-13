fb39de37f151e289f4af2d8f5a1449de9babbbe2
1689863234 <1689863234@qq.com>
2026-05-13 09:05:28 +0800

    refactor(EmptyLayout): 移除多余的keep-alive和路由key优化
    调整空布局组件，移除基于配置的缓存逻辑，改用fullPath作为组件key，简化代码逻辑

--- packages/base/src/frontend/layouts/EmptyLayout.vue
+++ packages/base/src/frontend/layouts/EmptyLayout.vue
@@ -1,21 +1,13 @@
 <template>
   <router-view v-slot="{ Component, route: viewRoute }">
-    <keep-alive v-if="keepAliveEnabled" :max="20">
-      <component :is="Component" :key="viewRoute.name" />
-    </keep-alive>
-    <component v-else :is="Component" :key="viewRoute.name" />
+    <component :is="Component" :key="viewRoute.fullPath" />
   </router-view>
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue';
 import { useRoute } from 'vue-router';
-import { useLayoutStore } from '../store/layout-store';
 
 defineOptions({ name: 'MfwEmptyLayout' });
 
 const route = useRoute();
-const layoutStore = useLayoutStore();
-
-const keepAliveEnabled = computed(() => layoutStore.styleConfig.keepAlive);
 </script>
